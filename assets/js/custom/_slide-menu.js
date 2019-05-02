6008fc903c366c6c97c609435a758e2b8b78702b
alexandra <alexaspalato@gmail.com>
2019-05-02 10:47:07 +0200

    close menu

--- assets/js/custom/_slide-menu.js
+++ assets/js/custom/_slide-menu.js
@@ -3,8 +3,13 @@ import { $ } from './_utilities';
 const trigger = $('.header-menu__trigger');
 const slideMenu = $('.slide-menu');
 const close = $('.slide-menu__close');
-function openMenu() {
-	return slideMenu.classList.add('isOpen');
-}
+const openMenu = () => slideMenu.classList.add('isOpen');
+const closeMenu = () => slideMenu.classList.remove('isOpen');
 
 trigger.on('click', openMenu);
+close.on('click', closeMenu);
+// document.on('click', e => {
+// 	if (!e.target.closest('.slide-menu')) {
+// 		closeMenu(e);
+// 	}
+// });
