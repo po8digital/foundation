0491e8a27b682366d04333dc41643a701a683fef
alexandra <alexaspalato@gmail.com>
2019-05-06 10:19:15 +0200

    related posts

--- single.php
+++ single.php
@@ -12,9 +12,18 @@
 $context = Timber::context();
 $timber_post = Timber::query_post();
 $context['post'] = $timber_post;
+// $cat = implode(get_the_category());
+$args = array(
+	'post_type' => 'post',
+	'posts_per_page' => 3,
+	// 'category_name' => $cat,
+);
 
-if ( post_password_required( $timber_post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
+//TODO: get the category and exclude this post
+$context['same_cat'] = Timber::get_posts($args);
+
+if (post_password_required($timber_post->ID)) {
+	Timber::render('single-password.twig', $context);
 } else {
-	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single.twig' ), $context );
+	Timber::render(array('single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single.twig'), $context);
 }
