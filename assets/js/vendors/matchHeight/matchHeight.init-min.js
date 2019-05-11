63493c945e7b1100af97ecd9f303c602d0cd4887
alexandra <alexaspalato@gmail.com>
2019-05-11 10:44:13 +0200

    lab responsive

--- assets/js/vendors/matchHeight/matchHeight.init-min.js
+++ assets/js/vendors/matchHeight/matchHeight.init-min.js
@@ -1,2 +1,2 @@
-!function(e){e(".featured-posts__item--wrap").matchHeight(),e(".featured-services__item--content").matchHeight()}(jQuery);
+!function(t){t(".featured-posts__item--wrap").matchHeight(),t(".featured-services__item--content").matchHeight(),t(".tease-post--small .tease-post__content--top").matchHeight()}(jQuery);
 //# sourceMappingURL=matchHeight.init-min.js.map
--- assets/js/vendors/matchHeight/matchHeight.init.js
+++ assets/js/vendors/matchHeight/matchHeight.init.js
@@ -1,4 +1,5 @@
 (function($) {
 	$('.featured-posts__item--wrap').matchHeight();
 	$('.featured-services__item--content').matchHeight();
+	$('.tease-post--small .tease-post__content--top').matchHeight();
 })(jQuery);
