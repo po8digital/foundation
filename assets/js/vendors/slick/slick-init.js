7b5d59b0dea50e91f5a7337e5a82bda027621a07
alexandra <alexaspalato@gmail.com>
2019-05-17 09:52:11 +0200

    resources

--- assets/js/vendors/slick/slick-init.js
+++ assets/js/vendors/slick/slick-init.js
@@ -1,8 +1,34 @@
 jQuery(document).ready(function($) {
 	$('.slider').slick({
-		autoplay: false,
+		autoplay: true,
 		speed: 1000,
 		arrow: false,
 		dots: true,
+	});
+
+	$('.resources__slider').slick({
+		autoplay: false,
+		speed: 1000,
+		arrow: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
 	});
 });
