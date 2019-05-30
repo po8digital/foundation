fb680dfb04755bfecf72a75cb32d75f723943414
Vinay Paudel <paudelvinay@gmail.com>
2019-05-30 13:39:31 +0545

    managing account setup and payment

--- select-plan.php
+++ select-plan.php
@@ -3,4 +3,13 @@
 
 $context   = Timber::get_context();
 $templates = array('select-plan.twig');
+if( isset($_GET["email_address"]) && $_GET["email_address"]!="" ){
+	$context["email_address"] = $_GET["email_address"];
+}
+if( isset($_GET["first_name"]) && $_GET["first_name"]!="" ){
+	$context["first_name"] = $_GET["first_name"];
+}
+if( isset($_GET["last_name"]) && $_GET["last_name"]!="" ){
+	$context["last_name"] = $_GET["last_name"];
+}
 Timber::render($templates, $context);
