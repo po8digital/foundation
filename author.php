270a2a5799e775d625d0d98237bb80263ba17556
alexandra <alexaspalato@gmail.com>
2019-06-22 10:48:55 +0200

    author intro structure

--- author.php
+++ author.php
@@ -15,8 +15,16 @@ $context          = Timber::context();
 $context['posts'] = new Timber\PostQuery();
 if (isset($wp_query->query_vars['author'])) {
 	$author            = new Timber\User($wp_query->query_vars['author']);
+	$author_id = get_the_author_meta('ID');
+
 	$context['author'] = $author;
-	$context['title']  = 'Author Archives: ' . $author->name();
+	$context['author_url'] = $author->link;
+	$context['author_avatar'] = $author->avatar;
+	$context['author_position'] = get_field('user_position', 'user_' . $author_id);
+	$context['author_fb'] = get_field('user_facebook', 'user_' . $author_id);
+	$context['author_linkedin'] = get_field('user_linkedin', 'user_' . $author_id);
+	$context['author_twitter'] = get_field('user_twitter', 'user_' . $author_id);
+	$context['title']  = 'Author Archives: ' . $author;
 	$context['cats'] = get_categories();
 }
 Timber::render(array('author.twig', 'archive.twig'), $context);
