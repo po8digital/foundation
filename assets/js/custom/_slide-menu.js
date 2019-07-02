bd4cd6a239e16e0d4a3d8d59711b0547366bc599
Eric Antonio <dev.antonio.eric@gmail.com>
2019-07-02 09:21:02 +0300

    Submenu arrow click corrigé

--- assets/js/custom/_slide-menu.js
+++ assets/js/custom/_slide-menu.js
@@ -4,11 +4,17 @@ const trigger = $('.header-menu__trigger');
 const slideMenu = $('.slide-menu');
 const close = $('.slide-menu__close');
 const itemsWithChildrens = $$('.slide-menu .menu-item-has-children');
+var $diff = 0;
 // const openChildrensTrigger;
 // console.log(dataset('after'));
 itemsWithChildrens.forEach(item =>
 	item.on('click', e => {
-		item.classList.toggle('active');
+		$diff = (jQuery(item).width() - e.offsetX);
+		if ( $diff <= 40 ) {
+			item.classList.toggle('active');
+		} else {
+			//console.log('clicked not after' + (jQuery(item).width() +' - '+ e.offsetX) +' = ' + $diff);
+		}
 	})
 );
 //TODO: doesn't work on mobile
