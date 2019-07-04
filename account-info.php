1f4a4762441ed5a3b8811bddd04b0cba6a896006
Vinay Paudel <paudelvinay@gmail.com>
2019-07-04 15:06:39 +0545

    membership issues version1

--- account-info.php
+++ account-info.php
@@ -2,7 +2,40 @@
 //Template name: Account info
 $context   = Timber::get_context();
 $templates = array('account-info.twig');
-$context['name'] = "Name";
-$context['email'] = "yourname@email.com";
+current_user = wp_get_current_user();
+$context['successmessage'] ="";
+if(isset($_REQUEST["success"]) && $_REQUEST["success"]==1){
+	$context['successmessage'] ="Your Information is successfully saved";
+}
+if( 0 != $current_user->ID ){
+	$context['name'] = $current_user->user_firstname." ".$current_user->user_lastname;
+	$context['email'] = $current_user->user_email;	
+	$entry_id = get_user_meta( $current_user->ID, 'entry_id', true );
+	$context['cc_details'] = get_user_meta( $current_user->ID, 'card number', true );
+	$context['cc_type'] = get_user_meta( $current_user->ID, 'card type', true );
+	$context['address'] = get_user_meta( $current_user->ID, 'address', true );
+	$context['city'] = get_user_meta( $current_user->ID, 'city', true );	
+	$context['postal'] = get_user_meta( $current_user->ID, 'postal', true );
+	$context['state'] = get_user_meta( $current_user->ID, 'state', true );
+	$context['country'] = get_user_meta( $current_user->ID, 'country', true );
+	$paid_plan = get_user_meta( $current_user->ID, 'user_paid_plan', true );
+	$context['paid_plan'] = $paid_plan;
+	if( $paid_plan =="plan1_subscriber" ){		
+		$context['paid_plan_id'] = 1;
+		$context['upgrade_plan_id'] = 2;
+		$context['upgrade_text'] = "If you change your plan you will be billed yearly instead of monthly. Saving $20/yr";
+		$context['active_plan_info'] = "PLAN 1 - $10/mo";
+		$context['upgradee_plan_info'] = "PLAN 2 - $100/yr";
+	} else {
+		$context['paid_plan_id'] = 2;
+		$context['upgrade_plan_id'] = 1;
+		$context['upgrade_text'] = "If you change your plan you will be billed monthly instead of yearly.";
+		$context['active_plan_info'] = "PLAN 2 - $100/yr";
+		$context['upgrade_plan_info'] = "PLAN 1 - $10/mo";
+	}
+	$context['cancel_link'] = site_url('/cancel-subscription/?plan=plan'.$context['paid_plan_id']);
+	$context['upgrade_link'] = site_url('/upgrade-subscription/?plan=plan'.$context['upgrade_plan_id']);
+	$context['logout_url'] =  wp_logout_url( home_url() );
+}
 //TODO: same for password and billing with the hidden parts
 Timber::render($templates, $context);
--- functions.php
+++ functions.php
@@ -157,3 +157,188 @@ if (function_exists('acf_add_options_page')) {
 }
 
 new StarterSite();
+
+//auto login after register and redirect to the corresponding class
+add_action( 'gform_user_registered', 'wpc_gravity_registration_autologin',  999, 4 );
+/**
+ * Auto login after registration.
+ */
+function wpc_gravity_registration_autologin( $user_id, $user_config, $entry, $password ) {
+  $user = get_userdata( $user_id );
+  $user_login = $user->user_login;
+  $user_password = $password;
+  #$user->set_role(get_option('default_role', 'subscriber'));
+    wp_signon( array(
+    'user_login' => $user_login,
+    'user_password' =>  $user_password,
+    'remember' => false
+
+    ) );
+    #wp_redirect(site_url( "/the-lab/" ));
+    #exit(); 
+}
+
+// Save the users' Stripe customer ID after a payment is made via a subscription Stripe feed.
+add_action( 'gform_stripe_customer_after_create', 'save_stripe_customer_id',99999 );
+function save_stripe_customer_id( $customer ) {
+	error_log(print_r("##MEM DEtails ##",true));
+    error_log(print_r($customer,true));
+    error_log(print_r(get_current_user_id(),true));
+    if ( is_user_logged_in () ) {
+        $user_id = get_current_user_id();
+        GFCommon::log_debug( __METHOD__ . '(): Adding customer id ' . $customer->id . ' to user ' . $user_id );
+        update_user_meta( $user_id, 'stripe_customer_id', $customer->id );
+    }
+}
+// Gets the current Stripe customer ID to change the billing details on. Tests if the user has an ID and is signed in.
+add_filter( 'gform_stripe_customer_id', 'get_stripe_customer_id',99999 );
+function get_stripe_customer_id( $customer_id ) {
+    if ( is_user_logged_in () &&  get_user_meta( get_current_user_id(), 'stripe_customer_id', true ) != ''){
+        $user_id = get_current_user_id();
+        $customer_id = get_user_meta( $user_id, 'stripe_customer_id', true );
+    }
+    return $customer_id;
+}
+ 
+// Make a form that has a Stripe Product and Services feed, and make sure no payment is made.
+add_filter( 'gform_stripe_charge_authorization_only', 'stripe_charge_authorization_only', 99999, 2 );
+function stripe_charge_authorization_only( $authorization_only, $feed ) {
+    $feed_name  = rgars( $feed, 'meta/feedName' );
+    // The name associated with the Stripe feed.
+    if ( $feed_name == 'Update Billing' ) {
+        return true;
+    }
+    return $authorization_only;
+}
+
+#add_filter("gform_field_value_email_address", "populate_email");
+function populate_email(){
+	global $current_user;
+	get_currentuserinfo();
+	$useremail = $current_user->user_email;
+	return $useremail;
+}
+
+//once its actually cancelled at the end of the period, downgrade users role
+add_action( 'gform_post_payment_callback', 'remove_user_privileges', 10, 3 );
+function remove_user_privileges( $entry, $action, $result ) {
+ if ( ! $result && rgar( $action, 'type' ) == 'cancel_subscription' && strtolower( $entry['payment_status'] ) == 'cancelled' ) { 
+    //$entry here is the original entry used to subscribe (form 2). not the cancel $entry. 
+     	cancel_user_role($entry);  
+  }
+ }
+
+function cancel_user_role($entry){
+	$user_id = get_current_user_id();
+	$u = new WP_User( $user_id );
+	$payment_amnt = $entry["payment_amount"];
+	$package_details ="";
+	foreach( $entry as $entry_val):				
+		if( strpos($entry_val, $payment_amnt)!==FALSE && strpos($entry_val, "|")!==FALSE ){
+			$package_details = $entry_val;
+			break;
+		}
+				
+	endforeach;
+	$package_details = explode("|",$package_details );
+	$user_package_id = $package_details[0];
+	if( $user_package_id !="" ){
+		if( $user_package_id == "Plan2" )
+			$user_role = "plan2_subscriber";
+		else
+			$user_role = "plan1_subscriber";
+		
+		if( get_role($user_role) )
+			$u->remove_role( $user_role );	
+	}
+}
+
+//assign role after successfull transaction
+add_action("gform_user_registered", "completed_package_registration", 10, 4);
+add_action("gform_user_updated", "completed_package_registration", 10, 4);
+
+function completed_package_registration($user_id, $config, $entry, $user_pass) {
+			
+			if( !empty($entry["source_url"]) && strpos($entry["source_url"], "plan")!==FALSE ){
+				parse_str( parse_url( $entry["source_url"], PHP_URL_QUERY), $pcg_array );
+				$user_package_id = $pcg_array["plan"] ;
+			} elseif( !empty($entry[10]) ){
+				$package_details = $entry[10];
+				$package_details = explode("|",$package_details );
+				$user_package_id = $package_details[0];
+			} else {
+				return ;
+			}
+
+			if( ( $user_package_id !=""  && strtolower($entry["payment_status"])=="active") || ($user_package_id !=""  && strtolower($entry["payment_status"])=="paid" && $entry["status"]=="active") ){
+				if( $user_package_id == "Plan2" )
+					$user_role = "plan2_subscriber";
+				else
+					$user_role = "plan1_subscriber";
+				$stripe_customer_id = gform_get_meta( $entry["id"], 'stripe_customer_id' );
+				update_user_meta( $current_user->ID, 'stripe_customer_id', $stripe_customer_id );
+				update_user_meta($current_user->ID, 'fnd_entry_id', $entry["id"]);
+				$u = new WP_User( $user_id );				
+        		if( get_role($user_role) ){
+        			$u->add_role( $user_role );
+        			update_user_meta( $user_id, 'user_paid_plan', $user_role);
+        		}
+
+			} 		
+
+}
+
+//process the subscription cancelling
+add_action( 'wp_ajax_fnd_gaddon_cancel_subscription', 'fnd_cancel_subscription_function' );
+function fnd_cancel_subscription_function(){
+	check_ajax_referer( 'gaddon_cancel_subscription', 'gaddon_cancel_subscription' );
+		$entry_id = $_POST['entry_id'];
+		$user_id = get_current_user_id();
+		$u = new WP_User( $user_id );
+		$entry = GFAPI::get_entry( $entry_id );
+		$form  = GFAPI::get_form( $entry['form_id'] );
+		$feed  = gf_stripe()->get_payment_feed( $entry, $form );
+
+ 		if ( empty ( $feed ) ) {
+ 			return;
+ 		}
+ 
+  if ( is_array( $feed ) && rgar( $feed, 'addon_slug' ) == 'gravityformsstripe'  ) {
+    	gf_stripe()->cancel_subscription( $entry, $feed );    
+   		//destroy old entry id so they cant cancel twice... not sure its necessary
+   		update_user_meta( $user_id, 'user_entry_id', '');
+   		delete_user_meta($user_id, 'user_package_id');
+   		delete_user_meta($user_id, 'user_entry_txn_type');
+   		delete_user_meta($user_id, 'user_entry_pmt_status');
+   		#do not downgrade user now
+   		#cancel_user_rolr($entry_id);
+   		#$u->set_role( 'subscriber' );		
+    	die( '1' );
+		} else {
+			die( '0' );
+		}	
+}
+
+//user can cancel subscription
+add_action( 'gform_after_submission_16', 'fnd_cancel_subscription', 10, 2 );
+function fnd_cancel_subscription( $entry, $form ) {
+ if( isset($_REQUEST["plan"]) && $_REQUEST[""]!="plan" ){
+	  //get original current users entry id from form 2. NOT this entry id!
+	  $entry_id = get_user_meta(get_current_user_id(), 'fnd_entry_id', true);
+	 
+	  //now cancel that entry's subscription
+	  $old_entry = GFAPI::get_entry( $entry_id );
+	  $feed = is_wp_error( $old_entry ) || ! function_exists( 'gf_stripe' ) ? false : gf_stripe()->get_payment_feed( $entry );
+	 
+	  if ( is_array( $feed ) && rgar( $feed, 'addon_slug' ) == 'gravityformsstripe' && gf_stripe()->cancel( $entry, $feed ) ) {
+	    gf_stripe()->cancel_subscription( $old_entry, $feed );
+	    
+	   //destroy old entry id so they cant cancel twice... not sure its necessary
+	   update_user_meta(get_current_user_id(), 'fnd_entry_id', '');
+
+	   //set meta to unsubscribed till period ends. in limbo state
+	   update_user_meta (get_current_user_id(), 'subscribed_till_end', 'yes');    
+  	} 
+ }
+ 
+}
